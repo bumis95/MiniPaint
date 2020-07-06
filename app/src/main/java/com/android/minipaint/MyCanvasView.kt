cf3fa18b8b4a1165e0d16efa174a3ceb9ba7cacd
bumis95 <bumis95@gmail.com>
2020-07-06 12:48:56 +0300

    Implement touchMove()

--- app/src/main/java/com/android/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/android/minipaint/MyCanvasView.kt
@@ -7,7 +7,9 @@ import android.graphics.Paint
 import android.graphics.Path
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import androidx.core.content.res.ResourcesCompat
+import kotlin.math.abs
 
 private const val STROKE_WIDTH = 12f
 
@@ -39,6 +41,8 @@ class MyCanvasView(context: Context) : View(context) {
 
     private var currentX = 0f
     private var currentY = 0f
+
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
 
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
         super.onSizeChanged(width, height, oldWidth, oldHeight)
@@ -76,7 +80,18 @@ class MyCanvasView(context: Context) : View(context) {
     }
 
     private fun touchMove() {
-        TODO("Not yet implemented")
+        val dx = abs(motionTouchEventX - currentX)
+        val dy = abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            // QuadTo() adds a quadratic bezier from the last point,
+            // approaching control point (x1,y1), and ending at (x2,y2).
+            path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+            // Draw the path in the extra bitmap to cache it.
+            extraCanvas.drawPath(path, paint)
+        }
+        invalidate()
     }
 
     private fun touchUp() {
